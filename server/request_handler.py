2af7e064a922ac3247f5a2f367ffef97326b826e
DanielCortild <daniel.cortild@gmail.com>
2020-12-04 14:57:28 +0100

    Removed server-side threading

--- server/request_handler.py
+++ server/request_handler.py
@@ -7,75 +7,60 @@ import json
 
 
 class Server(object):
-    PLAYERS = 2
+    PLAYERS = 1
 
     def __init__(self):
         self.connection_queue = []
         self.game_id = 0
+        # server = "188.166.107.89"
+        self.addr = ("localhost", 1286)
 
     def player_thread(self, conn, player):
         """
         Handles communication between clients
         :param conn: connection object
-        :param ip: str
-        :param name: str
+        :param player: Player
         :return: None
         """
         while True:
             try:
                 try:
-                    data = conn.recv(1024)
-                    data = json.loads(data.decode())
-                    # print(f"[LOG] Received Value : {data}")
-                except Exception as e:
+                    data = json.loads(conn.recv(1024).decode())
+                except:
                     break
 
-                keys = [int(key) for key in data.keys()]
-                send_msg = {key: [] for key in keys}
-
-                for key in keys:
-                    if key == -1: # Get game, returns list of players
-                        if player.game:
-                            send = {player.get_name(): player.get_score() for player in player.game.players}
-                            send_msg[-1] = send
-                        else:
-                            send_msg[-1] = []
+                key = int(list(data.keys())[0])
+                send_msg = ""
+                if key == -1: # Get game, returns list of players
                     if player.game:
-                        if key == 0:  # Guess
-                            correct = player.game.player_guess(player, data['0'][0])
-                            send_msg[0] = correct
-                        elif key == 1:  # Skip
-                            skip = player.game.skip()
-                            send_msg[1] = skip
-                        elif key == 2:  # Get chat
-                            content = player.game.round.chat.get_chat()
-                            send_msg[2] = content
-                        elif key == 3:  # Get board
-                            brd = player.game.board.get_board()
-                            send_msg[3] = brd
-                        elif key == 4:  # Get score
-                            scores = player.game.get_player_scores()
-                            send_msg[4] = scores
-                        elif key == 5:  # Get round
-                            rnd = player.game.round_counter
-                            send_msg[5] = rnd
-                        elif key == 6:  # Get word
-                            word = player.game.round.word
-                            send_msg[6] = word
-                        elif key == 7:  # Get skips
-                            skips = player.game.round.skips
-                            send_msg[7] = skips
-                        elif key == 8:  # Update board
-                            x, y, color = data['8'][:3]
-                            player.game.update_board(x, y, color)
-                        elif key == 9:  # Get round time
-                            t = player.game.round.time
-                            send_msg[9] = t
-                    if key == 10:
-                        raise Exception("Not valid request")
+                        send_msg = {
+                            player.get_name(): player.get_score() for player in player.game.players
+                        }
+                    else:
+                        send_msg = {}
+                if player.game:
+                    if key == 0:  # Guess
+                        send_msg = player.game.player_guess(player, data['0'][0])
+                    elif key == 1:  # Skip
+                        send_msg = player.game.skip()
+                    elif key == 2:  # Get chat
+                        send_msg = player.game.round.chat.get_chat()
+                    elif key == 3:  # Get board
+                        send_msg = player.game.board.get_board()
+                    elif key == 4:  # Get score
+                        send_msg = player.game.get_player_scores()
+                    elif key == 5:  # Get round
+                        send_msg = player.game.round_counter
+                    elif key == 6:  # Get word
+                        send_msg = player.game.round.word
+                    elif key == 7:  # Get skips
+                        send_msg = player.game.round.skips
+                    elif key == 8:  # Update board
+                        player.game.update_board(data['8'][:3])
+                    elif key == 9:  # Get round time
+                        send_msg = player.game.round.time
 
                 conn.sendall(json.dumps(send_msg).encode()+".".encode())
-
 
             except Exception as e:
                 print(f"[EXCEPTION] {player.get_name()}: {e}")
@@ -90,7 +75,7 @@ class Server(object):
 
     def handle_queue(self, player):
         """
-        Adds player to queue and creates new game if enough players
+        Adds given player to queue and starts new game if sufficient number of players
         :param player: Player
         :return: None
         """
@@ -98,59 +83,48 @@ class Server(object):
 
         if len(self.connection_queue) >= self.PLAYERS:
             game = Game(self.game_id, self.connection_queue[:])
-
             for p in game.players:
                 p.set_game(game)
-
             self.game_id += 1
             self.connection_queue = []
 
     def authentication(self, conn, addr):
         """
         Authentication here
-        :param addr: str
         :param conn: connection object
+        :param addr: str
         :return: None
         """
         try:
-            data = conn.recv(1024)
-            name = str(data.decode())
+            name = conn.recv(1024).decode()
             if not name:
                 raise Exception("No name received")
-            conn.sendall("Connected".encode())
+            conn.sendall("[CONNECTED]".encode())
 
             player = Player(addr, name)
             self.handle_queue(player)
-            thread = threading.Thread(target=self.player_thread, args=(conn, player))
-            thread.start()
+            threading.Thread(target=self.player_thread, args=(conn, player)).start()
         except Exception as e:
             print(f"[EXCEPTION] {e}")
             conn.close()
 
     def connected_thread(self):
-
-        # server = "188.166.107.89"
-        server = "localhost"
-        port = 1284
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            s.bind((server, port))
+            sock.bind(self.addr)
         except socket.error as e:
-            str(e)
+            print(f"[SOCKET ERROR] {e}")
 
-        s.listen(1)
-        print("Waiting for a connection, Server started")
+        sock.listen(1)
+        print("[START] Waiting for a connection")
 
         while True:
-            conn, addr = s.accept()
-            print("[CONNECT] New Connection")
-
+            conn, addr = sock.accept()
+            print(f"[CONNECT] {addr[0]}:{addr[1]}")
             self.authentication(conn, addr)
 
 
 if __name__ == "__main__":
     s = Server()
-    thread = threading.Thread(target=s.connected_thread)
-    thread.start()
+    # threading.Thread(target=s.connected_thread).start()
+    s.connected_thread()
--- test_connection.py
+++ test_connection.py
@@ -1,15 +1,12 @@
 import socket
 import json
-import time
 
 
 class Network:
     def __init__(self, name):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # self.server = "188.166.107.89"
-        self.server = "localhost"
-        self.port = 1284
-        self.addr = (self.server, self.port)
+        self.addr = ("localhost", 1286)
         self.name = name
         self.connect()
 
@@ -17,8 +14,7 @@ class Network:
         try:
             self.client.connect(self.addr)
             self.client.sendall(self.name.encode())
-            if not json.loads(self.client.recv(2048).decode()):
-                raise Exception("Connection Failed")
+            print(self.client.recv(1024).decode())
         except Exception as e:
             self.disconnect(e)
 
@@ -26,33 +22,25 @@ class Network:
         try:
             self.client.send(json.dumps(data).encode())
             d = ""
-            while True:
-                last = self.client.recv(1024).decode()
-                d += last
-                if d[-1] == ".":
-                    d = d[:-1]
-                    break
-            keys = [key for key in data.keys()]
-            return json.loads(d)[str(keys[0])]
-        except socket.error as e:
+            while d.count(".") == 0:
+                d += self.client.recv(1024).decode()
+            return json.loads(d.replace(".", ""))
+        except Exception as e:
             self.disconnect(e)
 
     def disconnect(self, msg):
-        print(f"[EXCEPTION] Disconnected from server {msg}")
+        print(f"[EXCEPTION] Disconnected because {msg}")
         self.client.close()
 
-n = Network("Daniel Test")
-print(n.send({-1: []}))
-print(n.send({0: ["Pizza"]}))
-print(n.send({1: []}))
-print(n.send({2: []}))
-print(n.send({4: []}))
-print(n.send({5: []}))
-print(n.send({6: []}))
-print(n.send({7: []}))
-print(n.send({9: []}))
 
-
-# Only 8 has not been tested (Update Board)!!
-# 5:25:20
-# https://www.youtube.com/watch?v=wDIQ17T3sRk&list=WL&index=1&t=14429s&ab_channel=TechWithTim
+if __name__ == "__main__":
+    n = Network("Daniel Test")
+    print(n.send({-1: []}))
+    print(n.send({0: ["Pizza"]}))
+    # print(n.send({1: []}))
+    print(n.send({2: []}))
+    print(n.send({4: []}))
+    print(n.send({5: []}))
+    print(n.send({6: []}))
+    print(n.send({7: []}))
+    print(n.send({9: []}))
